30162d3c9583e197a5813aaf6fb9caccb087aa03
Samir El Hassan <samirelhassann@gmail.com>
2024-01-31 19:08:16 -0300

    Updates

---
+++ precache-manifest.8a2b6fba781c162722ae0f4b9f5a2a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b140c3aea7c979555f0371e2fcef2b2f",
+    "url": "/devsfood/index.html"
+  },
+  {
+    "revision": "01c8cd04ea8dc9dfe712",
+    "url": "/devsfood/static/css/main.860a9de9.chunk.css"
+  },
+  {
+    "revision": "899786a213eabac39ccb",
+    "url": "/devsfood/static/js/2.cb25b0f6.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/devsfood/static/js/2.cb25b0f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c8cd04ea8dc9dfe712",
+    "url": "/devsfood/static/js/main.f6b6000f.chunk.js"
+  },
+  {
+    "revision": "5a199c7cda9c419c7ba7",
+    "url": "/devsfood/static/js/runtime-main.27206ffa.js"
+  }
+]);
